ab68af7ac5a2b162e9860f8d4ee8c211c3eb9d15
Adrian <adrian_zinko@op.pl>
2020-06-17 23:07:22 +0200

    feat: display fetched data in ui using material ui cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import classes from "./Cards.module.css";
 
-const Cards = () => {
-    return (
-        <div>
-            <h1>Cards</h1>
-        </div>
-    );
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+	if (!confirmed) {
+		return "Loading...";
+	}
+
+	return (
+		<div className={classes.container}>
+			<Grid container spacing={3} justify="center">
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							{confirmed.value}
+						</Typography>
+						<Typography variant="h5">DATA</Typography>
+						<Typography color="textSecondary">DATE</Typography>
+						<Typography variant="body2">
+							Number of active cases of COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							{recovered.value}
+						</Typography>
+						<Typography variant="h5">DATA</Typography>
+						<Typography color="textSecondary">DATE</Typography>
+						<Typography variant="body2">
+							Number of recoveries from COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							{deaths.value}
+						</Typography>
+						<Typography variant="h5">DATA</Typography>
+						<Typography color="textSecondary">DATE</Typography>
+						<Typography variant="body2">
+							Number of deaths from COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default Cards;
