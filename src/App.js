b658d1739cf3b3f60fbb74299afda8dedb1b0a7e
Adrian <adrian_zinko@op.pl>
2020-06-17 15:44:44 +0200

    create api file for calling the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 
 import classes from "./App.module.css";
 
 class App extends Component {
+	async componentDidMount() {
+		const data = await fetchData();
+
+		console.log(data);
+	}
+
 	render() {
 		return (
 			<div className={classes.App}>
