5e616060aaa5d9781e066a67ca3bdf3c732c8bb1
Adrian <adrian_zinko@op.pl>
2020-06-18 10:27:03 +0200

    feat: create async func for fetch countries data

--- src/api/index.js
+++ src/api/index.js
@@ -25,5 +25,17 @@ export const fetchDailyData = async () => {
 		}));
 
 		return modifiedData;
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const countries = async () => {
+	try {
+		const response = await axios.get(`${url}/countries`);
+
+		console.log(response);
+	} catch (error) {
+		console.log(error);
+	}
 };
