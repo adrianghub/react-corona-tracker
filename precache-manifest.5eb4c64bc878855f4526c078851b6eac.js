d58d146a445de3505491a093d5d4ae3493fc5749
Adrian <adrian_zinko@op.pl>
2020-06-18 12:50:39 +0200

    Updates

---
+++ precache-manifest.5eb4c64bc878855f4526c078851b6eac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13c576ca0d9364de301660c7a1c0541",
+    "url": "/adrianghub/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "af3ba98c75b147465272",
+    "url": "/adrianghub/react-corona-tracker/static/css/main.4c40f8e7.chunk.css"
+  },
+  {
+    "revision": "50e92081d91f1525049b",
+    "url": "/adrianghub/react-corona-tracker/static/js/2.8b208653.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/adrianghub/react-corona-tracker/static/js/2.8b208653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3ba98c75b147465272",
+    "url": "/adrianghub/react-corona-tracker/static/js/main.8781b005.chunk.js"
+  },
+  {
+    "revision": "bed271c8005a3879c7ef",
+    "url": "/adrianghub/react-corona-tracker/static/js/runtime-main.a5c0c947.js"
+  },
+  {
+    "revision": "0e00dcbd4515d420d9350f9c07c4fdf0",
+    "url": "/adrianghub/react-corona-tracker/static/media/corona.0e00dcbd.png"
+  }
+]);
