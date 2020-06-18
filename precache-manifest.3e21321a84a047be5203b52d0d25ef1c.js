5cf50ac297b1ef483b333bc9759938ef67065253
Adrian <adrian_zinko@op.pl>
2020-06-18 20:04:07 +0200

    Updates

---
+++ precache-manifest.3e21321a84a047be5203b52d0d25ef1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c0a1e56163e838a8ba8bc9dad814c9",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "92656eb18d598852f4a3",
+    "url": "/react-corona-tracker/static/css/main.4c40f8e7.chunk.css"
+  },
+  {
+    "revision": "54431ab8a7c4253b9afc",
+    "url": "/react-corona-tracker/static/js/2.077692ed.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker/static/js/2.077692ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92656eb18d598852f4a3",
+    "url": "/react-corona-tracker/static/js/main.1901d245.chunk.js"
+  },
+  {
+    "revision": "cd06ae12231c84d4f78f",
+    "url": "/react-corona-tracker/static/js/runtime-main.6115be80.js"
+  },
+  {
+    "revision": "0e00dcbd4515d420d9350f9c07c4fdf0",
+    "url": "/react-corona-tracker/static/media/corona.0e00dcbd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6c9510ba302a0f7685a6ffa30416385.js"
+  "/react-corona-tracker/precache-manifest.3e21321a84a047be5203b52d0d25ef1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-corona-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
